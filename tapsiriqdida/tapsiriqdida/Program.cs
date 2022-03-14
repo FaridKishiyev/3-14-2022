b87e32fa8f7c93a417b86ddf7996c42f2a06bf8d
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-03-14 11:25:54 +0400

    Update Program.cs

--- tapsiriqdida/tapsiriqdida/Program.cs
+++ tapsiriqdida/tapsiriqdida/Program.cs
@@ -6,12 +6,12 @@ namespace class_vehiclecar
     {
         static void Main(string[] args)
         {
-            Car Hyundai = new Car("Mercedes", "AMG", "Black", 2008, 80, 0.3, 30);
-            Hyundai.ShowInfo();
+            Car cars = new Car("Mercedes", "AMG", "Black", 2008, 80, 0.3, 30);
+            cars.ShowInfo();
             Console.WriteLine("way to go");
             string input = Console.ReadLine();
             double distance = Convert.ToDouble(input);
-            Console.WriteLine($"Current Fuel:{Hyundai.Drive(distance).ToString("0.000")}l");
+            Console.WriteLine($"Current Fuel:{cars.Drive(distance)} liter");
         }
     }
 }
